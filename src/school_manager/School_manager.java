01ff91b4b77be51b291532ac260bad02ea4081bb
mohammed moneer <itxn13@gmail.com>
2025-12-09 00:32:38 +0300

    moha

--- src/school_manager/School_manager.java
+++ src/school_manager/School_manager.java
@@ -5,12 +5,15 @@ package school_manager;
 public class School_manager {
 
     public static void main(String[] args) {
-        System.out.println("hellow moaied");
-<<<<<<< HEAD
+        System.out.println("hellow mohammed");
+
       System.out.println("mohammed");
-=======
-    
->>>>>>> 652e4f72cf4dc5e502c69396a34b685c28856e87
+
+     System.out.println("mohammed moneer ");
+      System.out.println("ali");
+
+
+
         System.out.println("moayad");
         System.out.println("kjkdofjgkjkfgljelkrjglkejrkogjekorjgkjrokgmdlfkgjoeirj");
     

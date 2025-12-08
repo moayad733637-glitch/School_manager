ff1bde08f6aeda30c7f074869114399c16beff9a
mohammed moneer <itxn13@gmail.com>
2025-12-08 23:45:16 +0300

    bshz

---
+++ src/school_manager/School_manager.java
@@ -0,0 +1,15 @@
+
+package school_manager;
+
+
+public class School_manager {
+
+    public static void main(String[] args) {
+        System.out.println("hellow moaied");
+      System.out.println("mohammed");
+        System.out.println("moayad");
+        System.out.println("kjkdofjgkjkfgljelkrjglkejrkogjekorjgkjrokgmdlfkgjoeirj");
+    
+    }
+    
+}
